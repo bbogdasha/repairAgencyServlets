88e6ebb7ef9b1a6900b2875fd67187777057fcb6
Bogdan <bogdan4888@gmail.com>
2021-05-14 23:41:30 +0300

    startup tomcat with jetty

---
+++ src/main/java/com/bogdan/HomeServlet.java
@@ -0,0 +1,21 @@
+package com.bogdan;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+
+        printWriter.println("<html>");
+        printWriter.println("<h1>Hello!</h1>");
+        printWriter.println("</html>");
+    }
+}
---
+++ src/main/webapp/pages/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My first paragraph.</p>
+    </body>
+</html>
